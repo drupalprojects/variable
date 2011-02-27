96d6a95b5b97518dc23217f2e9aaf6c5f7b8623c
Jose Antonio Reyero del Prado <drupal@reyero.net>
2011-02-27 12:43:30 +0100

    Added documentation, some more comments, and cleaned up system variables.

--- variable.api.php
+++ variable.api.php
@@ -19,27 +19,50 @@
 /**
  * Define variables used by a module.
  * 
+ * Provides meta-information for each variable that includes at the very least some human readable title.
+ * This information may be used by other modules to select variables from a list for translating, exporting, etc.
+ * 
+ * Though not required we can also provide some more information to be able to handle the variable in an effective
+ * way, like which type of data and form element it uses, default value, etc.. There are multiple predefined 
+ * variable types ('type' attribute) that will add a predefined set of properties. Some of them are:
+ * 
+ * - "string": Simple plain string variable. The form element will be a text field and it will be localizable.
+ * - "number": Simple numeric value. The form element will be a text field.
+ * - "boolean": Simple TRUE/FALSE value. It will be a checkbox.
+ * - "enable": Enabled / Disabled selector. It will display as two radio buttons. 
+ * - "select": Selectable list of options. Depending on the number of options, the element will be a list of
+ *   radios or a drop down.
+ * - "options": List of options with multiple choices. It will be a list of checkboxes.
+ * ...
+ * 
+ * More variable types can be defined by modules using hook_variable_type_info().
+ * 
  * For the case of variable names that depend on some other parameter (like variables per content-type),
  * there's some special type of variables: Multiple variables. These can be defined like this:
  * 
- * $variables['node_options_[node_type]'] = array(
- *   'type' => 'multiple',
- *   'title' => t('Default options', array(), $options),
- *   'repeat' => array(
- *     'type' => 'options',
- *     'default' => array('status', 'promote'),
- *     'options callback' => 'node_variable_option_list',
- *   ),
- * );
+ * @code
+ *   $variables['node_options_[node_type]'] = array(
+ *     'type' => 'multiple',
+ *     'title' => t('Default options', array(), $options),
+ *     'repeat' => array(
+ *       'type' => 'options',
+ *       'default' => array('status', 'promote'),
+ *       'options callback' => 'node_variable_option_list',
+ *     ),
+ *   );
+ * @endcode
  * 
  * This multiple variable will spawn into one variable for each node type. Note the variable name that includes
- * the property [node_type]. Values for [node_type] will be defined on hook_variable_option_info().
+ * the property [node_type]. Values for [node_type] will be defined on hook_variable_type_info().
  * 
  * The 'repeat' property defines the properties of children variables. In this case the 'type' property is optional
  * and will default to 'multiple'.
  * 
  * @param $options
- *   'language' => Language object for which strings and defaults must be returned
+ *   Array of options to build variable properties. Since variable properties are cached per language
+ *   these options should be used at the very least for string translations, so titles and defaults are
+ *   localized. Possible options: 
+ *   - "language" => Language object for which strings and defaults must be returned. This one will be always defined.
  * 
  * @return
  *   An array of information defining the module's variables. The array
@@ -59,13 +82,58 @@
  *   - "element callback": Function to invoke to get a form element for this variable.
  *   - "module": Module to which this variable belongs. This property will be added automatically.
  *   - "repeat": Array of variable properties for children variables.
+ *   - "localize": Boolean value, TRUE for variables that should be localized. This may be used by other modules.
  */
 function hook_variable_info($options) {
-  
+  $variables['site_name'] = array(
+    'type' => 'string',
+    'title' => t('Name', array(), $options),
+    'default' => 'Drupal',
+    'description' => t('The name of this website.', array(), $options),
+    'element' => array('#required' => TRUE),
+  );
+  $variables['site_403'] = array(
+    'type' => 'drupal_path',
+    'title' => t('Default 403 (access denied) page', array(), $options),
+    'default' => '',
+    'description' => t('This page is displayed when the requested document is denied to the current user. Leave blank to display a generic "access denied" page.', array(), $options),
+  );
+  return $variables;  
 }
  
 /**
- * Define types of variables used by a module.
+ * Define types of variables or list of values used by a module.
+ * 
+ * These subtypes can be used to provide defaults for all properties of variables of this type
+ * or to provide a list of options either for variable options (selectable values) or for children
+ * variables in the case of multiple variables.
+ * 
+ * Example, three usages of variable type:
+ * @code
+ *   // Use variable type 'weekday' to provide a selector for a day of the week
+ *   $variables['date_first_day'] = array(
+ *   	 'type' => 'weekday',
+ *   	 'title' => t('First day of week'),
+ *   	 'default' => 0,
+ * 	 );
+ * 
+ *   // Use 'options' with value 'weekday' for any other variable that needs to provide a selectable
+ *   // list of days of the week. In this example you can select one or more days.
+ *   $variables['working_days'] = array(
+ *   	'type' => 'options',
+ *    'options' => 'weekday',
+ *    'title' => t('Select working days from the list.'),
+ *   );
+ *   
+ *   // Use 'multiple' with value 'weekday' to create a subset of variables, one for each day of the week.
+ *   // In fact, using '[weekday]' in the variable name will set these properties ('type' and 'multiple') automatically.
+ * 	 $variables['daily_greeting_[weekday]'] = array(
+ *      'type' => 'multiple',
+ * 			'multiple' => 'weekday',
+ *      'repeat' => array('type' => 'string'),
+ *      'title' => t('Greeting to display each day of the week'),
+ * 	 );
+ * @endcode
  * 
  * @return
  *   An array of information defining variable types. The array contains
@@ -125,31 +193,4 @@ function hook_variable_group_info() {
     'description' => t('Feed settings'),
     'access' => 'administer site configuration',
   );  
-}
-
-/**
- * Define lists to be used for variable options or multiple variables.
- * 
- * These lists can be used for variable options or for children of multiple variables.
- * 
- * @return
- *   An array of information defining lists of options. The array contains
- *   a sub-array for each variable options, with the list name as the key.
- * 
- *   Possible attributes:
- *   - "title": The human readable name of the list of options. Must be localized.
- *   - "callback": A function to invoke to get the list of options.
- *   - "cache": Whether this list is cacheable, defaults to FALSE.
- */
-function hook_variable_option_info() {
-  $options['weekday'] = array(
-    'title' => t('Day of week'),
-    'callback' => 'system_variable_option_weekday',
-  );
-  $options['theme'] = array(
-    'title' => t('Theme'),
-    'callback' => 'system_variable_option_theme',
-    'cache' => TRUE,
-  );
-  return $options;  
 }
